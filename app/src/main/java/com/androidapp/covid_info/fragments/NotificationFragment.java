a1b71ff4467002ed508fdfcf81172a788cf0af81
rajeshbhaskariyer <rajeshbhaskar96@gmail.com>
2021-11-25 17:26:52 +0530

    nothing

--- app/src/main/java/com/androidapp/covid_info/fragments/NotificationFragment.java
+++ app/src/main/java/com/androidapp/covid_info/fragments/NotificationFragment.java
@@ -59,10 +59,7 @@ public class NotificationFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
--- app/src/main/java/com/androidapp/covid_info/fragments/SmsFragment.java
+++ app/src/main/java/com/androidapp/covid_info/fragments/SmsFragment.java
@@ -29,8 +29,7 @@ public class SmsFragment extends Fragment {
     @BindView(R.id.webview)
     WebView webview;
     Activity context;
-    private String mParam1;
-    private String mParam2;
+
     public SmsFragment() {
         // Required empty public constructor
     }
@@ -42,10 +41,7 @@ public class SmsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
